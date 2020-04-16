0d3d2087bbae4fdf1367575dc24f9332696b18b4
Felix Fontein <felix@fontein.de>
2020-04-16 21:49:11 +0200

    Improve linting: don't die on non-dict file.

--- src/changelog.py
+++ src/changelog.py
@@ -20,6 +20,11 @@ import sys
 import yaml
 
 try:
+    from collections.abc import Mapping
+except ImportError:
+    from collections import Mapping
+
+try:
     import argcomplete
 except ImportError:
     argcomplete = None
@@ -315,29 +320,33 @@ class ChangelogFragmentLinter(object):
         """
         errors = []
 
-        for section, lines in fragment.content.items():
-            if section == self.config.prelude_name:
-                if not isinstance(lines, string_types):
-                    errors.append((fragment.path, 0, 0, 'section "%s" must be type str not %s' % (section, type(lines).__name__)))
-            else:
-                # doesn't account for prelude but only the RM should be adding those
-                if not isinstance(lines, list):
-                    errors.append((fragment.path, 0, 0, 'section "%s" must be type list not %s' % (section, type(lines).__name__)))
+        if isinstance(fragment.content, Mapping):
+            for section, lines in fragment.content.items():
+                if section == self.config.prelude_name:
+                    if not isinstance(lines, string_types):
+                        errors.append((fragment.path, 0, 0, 'section "%s" must be type str not %s' % (section, type(lines).__name__)))
+                else:
+                    # doesn't account for prelude but only the RM should be adding those
+                    if not isinstance(lines, list):
+                        errors.append((fragment.path, 0, 0, 'section "%s" must be type list not %s' % (section, type(lines).__name__)))
 
-                if section not in self.config.sections:
-                    errors.append((fragment.path, 0, 0, 'invalid section: %s' % section))
+                    if section not in self.config.sections:
+                        errors.append((fragment.path, 0, 0, 'invalid section: %s' % section))
 
-            if isinstance(lines, list):
-                for line in lines:
-                    if not isinstance(line, string_types):
-                        errors.append((fragment.path, 0, 0, 'section "%s" list items must be type str not %s' % (section, type(line).__name__)))
-                        continue
+                if isinstance(lines, list):
+                    for line in lines:
+                        if not isinstance(line, string_types):
+                            errors.append((fragment.path, 0, 0, 'section "%s" list items must be type str not %s' % (section, type(line).__name__)))
+                            continue
 
-                    results = rstcheck.check(line, filename=fragment.path, report_level=docutils.utils.Reporter.WARNING_LEVEL)
+                        results = rstcheck.check(line, filename=fragment.path, report_level=docutils.utils.Reporter.WARNING_LEVEL)
+                        errors += [(fragment.path, 0, 0, result[1]) for result in results]
+                elif isinstance(lines, string_types):
+                    results = rstcheck.check(lines, filename=fragment.path, report_level=docutils.utils.Reporter.WARNING_LEVEL)
                     errors += [(fragment.path, 0, 0, result[1]) for result in results]
-            elif isinstance(lines, string_types):
-                results = rstcheck.check(lines, filename=fragment.path, report_level=docutils.utils.Reporter.WARNING_LEVEL)
-                errors += [(fragment.path, 0, 0, result[1]) for result in results]
+
+        else:
+            errors.append((fragment.path, 0, 0, 'file must be a mapping not %s' % (type(fragment.content).__name__, )))
 
         return errors
 
